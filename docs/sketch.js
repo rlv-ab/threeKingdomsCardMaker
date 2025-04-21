9b6da2ef30456825d05fb2d93625c99768f63808
rlv-ab <rlvvmc10lv@gmail.com>
2025-04-21 14:08:26 +0000

    Add files via upload

--- docs/sketch.js
+++ docs/sketch.js
@@ -41,6 +41,12 @@ function textSkill(card,skillText,sx,sy,mh,mw,skill) {
           card.textFont(skillTextFontBold);
           card.fill(234,0,0);
         }
+      if(wordStr.includes('@@'))
+      {
+        wordStr = wordStr.replace('@@','');
+        card.textFont(skillTextFontBold);
+        card.fill(0,0,243);
+      }
       if(wordStr.includes('&&'))
       {
         if(wordStr.includes('spades'))
@@ -77,7 +83,7 @@ function textSkill(card,skillText,sx,sy,mh,mw,skill) {
       if(wordStr.includes('\n'))
         {
           linectr++;
-          y += 21;
+          y += 26;
           if(p<1)
           {
             x = sx+50
@@ -100,7 +106,7 @@ function textSkill(card,skillText,sx,sy,mh,mw,skill) {
       if ((x > mw - nextWordStrWidth) && (nextWordStr.includes('\n') == false)) 
       {
           linectr++;
-          y += 21
+          y += 26
           if(p<1) 
           {
             x = sx+50
@@ -110,22 +116,23 @@ function textSkill(card,skillText,sx,sy,mh,mw,skill) {
       }
 	}
   let offset;
-  if(linectr<=2)
+  if(linectr<=1)
   {
-    offset=62;
+    offset=58;
   }
   else
   {
-    offset=32;
+    offset=58;
   }
   console.log(linectr);
   skill.setStartPos(sy);
   skill.setPos((y+offset));
 }
 
-var frame,cardArt,skillPlate;
+var frame,cardArt,skillPlate,modifyPlate,UnlockPlate;
+var artOffset;
 var loadedImages =0;
-var skill1,skill2,skill3,skill4;
+var skill1,skill2,skill3,skill4,skill5,skill6;
 var skillArr;
 var chName, art,hp,faction
 
@@ -157,13 +164,18 @@ function preload()
   skillTextFont = loadFont('assets/Quadrat-Serial Regular.ttf')
   skillTitleFont = loadFont('assets/Impuls BT.ttf')
   nameFont = loadFont('assets/Mistral Regular.ttf')
+  modifyPlate = loadImage('assets/ModifyPlate.png')
+  UnlockPlate = loadImage('assets/UnlockPlate.png')
 }
 
 var in_faction,in_chname, in_hp, in_art, savebtn,
-in_skill1,in_skill1name,
-in_skill2,in_skill2name,
-in_skill3,in_skill3name,
-in_skill4,in_skill4name;
+art_uncropped, plateArr,
+in_skill1,in_skill1name, in_plate1,
+in_skill2,in_skill2name, in_plate2,
+in_skill3,in_skill3name, in_plate3,
+in_skill4,in_skill4name, in_plate4,
+in_skill5,in_skill5name, in_plate5,
+in_skill6,in_skill6name, in_plate6;
 function textBox(name,id,posX,posY,BW,BH)
 {
   if(name != 'noLabel')
@@ -191,12 +203,12 @@ function dropDown(id,posX,posY,BW,BH,type)
 {
   let label = createElement('label')
   label.id(id+'_label');
-  document.getElementById(id+'_label').innerHTML = "Faction";
+  document.getElementById(id+'_label').innerHTML = type;
   label.style('position','absolute');
   label.style('left',posX+'px');
   label.style('top',(posY-25)+'px');
   let ref;
-  if(type == 'faction')
+  if(type == 'Faction')
   {
     ref = createElement('select',
       ['<option value=\"Qun\">Qun</option>',
@@ -206,14 +218,22 @@ function dropDown(id,posX,posY,BW,BH,type)
        '<option value=\"AB\">Applebread</option>'
       ]);
   }
-  else
-  {
-    ref = createElement('select',
-      ['<option value=\"3\">3 HP</option>',
-       '<option value=\"4\">4 HP</option>',
-       '<option value=\"5\">5 HP</option>',
-      ]);
-  }
+  else if(type == 'Health')
+    {
+      ref = createElement('select',
+        ['<option value=\"3\">3 HP</option>',
+         '<option value=\"4\">4 HP</option>',
+         '<option value=\"5\">5 HP</option>',
+        ]);
+    }
+    else
+    {
+      ref = createElement('select',
+        ['<option value=\"Normal\">Normal</option>',
+         '<option value=\"Modify\">Modify</option>',
+         '<option value=\"Unlock\">Unlock</option>',
+        ]);
+    }
   ref.style('position','absolute');
   ref.style('left',posX+'px');
   ref.style('top',posY+'px');
@@ -227,26 +247,47 @@ function setup() {
   fullCard = createGraphics(w,h);
   skill1 = new Skill(),skill2 = new Skill(),
   skill3 = new Skill(),skill4 = new Skill();
-  skillArr = [skill1,skill2,skill3,skill4];
-  in_skill1 = textBox('Skill 1','input1',100,350,300,100)
-  in_skill1name = textBox('noLabel','input1Label',150,314,250,25)
-  in_skill2 = textBox('Skill 2','input2',100,500,300,100)
-  in_skill2name = textBox('noLabel','input2Label',150,465,250,25)
-  in_skill3 = textBox('Skill 3','input3',100,650,300,100)
-  in_skill3name = textBox('noLabel','input3Label',150,615,250,25)
-  in_skill4 = textBox('Skill 4','input4',100,800,300,100)
-  in_skill4name = textBox('noLabel','input3Label',150,765,250,25)
-  in_chname = textBox('Character Name',"charNameInput",100,150,300,40)
+  skill5 = new Skill(),skill6 = new Skill();
+  skillArr = [skill1,skill2,skill3,skill4,skill5,skill6];
+  in_skill1 = textBox('noLabel','input1',50,400,200,100)
+  in_skill1name = textBox('noLabel','input1Label',125,364,125,25)
+  in_plate1 = dropDown('type1',50,364,70,32,'Skill 1')
+  in_skill2 = textBox('noLabel','input2',270,400,200,100)
+  in_skill2name = textBox('noLabel','input2Label',345,364,125,25)
+  in_plate2 = dropDown('type2',270,364,70,32,'Skill 2')
+  in_skill3 = textBox('nolabel','input3',50,600,200,100)
+  in_skill3name = textBox('noLabel','input3Label',125,564,125,25)
+  in_plate3 = dropDown('type3',50,564,70,32,'Skill 3')
+  in_skill4 = textBox('noLabel','input4',270,600,200,100)
+  in_skill4name = textBox('noLabel','input4Label',345,564,125,25)
+  in_plate4 = dropDown('type4',270,564,70,32,'Skill 4')
+  in_skill5 = textBox('nolabel','input5',50,800,200,100)
+  in_skill5name = textBox('noLabel','input5Label',125,764,125,25)
+  in_plate5 = dropDown('type5',50,764,70,32,'Skill 5')
+  in_skill6 = textBox('noLabel','input6',270,800,200,100)
+  in_skill6name = textBox('noLabel','input6Label',345,764,125,25)
+  in_plate6 = dropDown('type6',270,764,70,32,'Skill 6')
+  in_chname = textBox('Character Name',"charNameInput",50,150,300,40)
+  plateArr = [in_plate1,in_plate2,in_plate3,in_plate4,in_plate5,in_plate6];
   in_art = createFileInput(handleImage)
   let filelabel = createElement('fileLabel')
   filelabel.id('file_label');
   document.getElementById('file_label').innerHTML = 'Character Art';
   filelabel.style('position','absolute');
-  filelabel.style('left','100px');
+  filelabel.style('left','50px');
   filelabel.style('top','210px');
-  in_art.position(100,230,);
-  in_faction = dropDown('fac',100,50,150,50,'faction')
-  in_hp = dropDown('fac',260,50,150,50,'health')
+  in_art.position(50,230);
+  in_faction = dropDown('fac',50,50,150,50,'Faction')
+  in_hp = dropDown('hp',210,50,150,50,'Health')
+  let offsetLabel = createElement('offsLabel')
+  offsetLabel.id('offs_label');
+  document.getElementById('offs_label').innerHTML = 'Art Position';
+  offsetLabel.style('position','absolute');
+  offsetLabel.style('left','450px');
+  offsetLabel.style('top','30px');
+  artOffset = createSlider(0,255);
+  artOffset.position(450,50);
+  artOffset.input(() => {refresh();});
   in_faction.changed(updateImages);
   in_art.changed(updateImages);
   in_hp.changed(updateImages);
@@ -255,11 +296,11 @@ function setup() {
   savebtn.style('width','150px');
   savebtn.style('height','150px');
   savebtn.mousePressed(saveCard);
-  savebtn.position(460,200);
+  savebtn.position(460,100);
 
   frame = getFrame(in_faction.value(),in_hp.value());
   skillPlate = loadImage('assets/SkillPlateQun.png',(loadedPlate)=>{skillPlate=loadedPlate; refresh();})
-  loadImage('assets/dummy.png',(loadedImage)=>{cardArt=loadedImage; refresh();})
+  loadImage('assets/dummy.png',(loadedImage)=>{art_uncropped=loadedImage; refresh();})
 }
 
 function saveCard()
@@ -274,9 +315,8 @@ function handleImage(file)
     
     loadImage(file.data,(loadedImage)=>
       {
-        cardArt=loadedImage;
-        cardArt.resize(0,910);
-        cardArt = cardArt.get(0,0,630,h);
+        art_uncropped=loadedImage;
+        art_uncropped.resize(0,910);
         refresh();
       })
   }
@@ -303,8 +343,13 @@ function refresh()
   skill3.setText(in_skill3.value())
   skill4.setTitle(in_skill4name.value());
   skill4.setText(in_skill4.value());
+  skill5.setTitle(in_skill5name.value());
+  skill5.setText(in_skill5.value());
+  skill6.setTitle(in_skill6name.value());
+  skill6.setText(in_skill6.value());
   //resize card art and add it
   fullCard.rect(15,15,640,910);
+  cardArt = art_uncropped.get(0+artOffset.value(),0,630,h);
   fullCard.image(cardArt,10,20);
   //create skill text boxes
   pg = createGraphics(boxW, h);
@@ -327,22 +372,44 @@ function refresh()
   //add card frame
   fullCard.image(frame,0,0,posx,posy);
   //add skill plates & title
-  skillPlate.resize(160,0);
+  let ttoffs = 0;
   for(let t=0;t<skillArr.length;t++)
     {
       if(skillArr[t].filled != 0)
       {
-        fullCard.image(skillPlate,textX-50,h-(textPos-skillArr[t].startPos+18));
+        let plt;
+        if(plateArr[t].value() == 'Modify')
+        {
+          plt = modifyPlate;
+          ttoffs = -38
+        }
+        else if(plateArr[t].value() == 'Unlock')
+        {
+          plt = UnlockPlate;
+          ttoffs = -38;
+        }
+        else plt = skillPlate;
+        plt.resize(160,0);
+        fullCard.image(plt,textX-50,h-(textPos-skillArr[t].startPos+18));
         fullCard.textFont(skillTitleFont);
         fullCard.textSize(30);
         fullCard.fill(255);
         fullCard.stroke(0);
         fullCard.strokeWeight(5);
-        fullCard.textAlign(CENTER);
+        if(ttoffs != 0)
+          {
+            fullCard.textAlign(LEFT);
+          }
+          else fullCard.textAlign(CENTER);
         fullCard.textLeading(24);
         let offset=0;
+        if(plateArr[t].value() == 'Modify' || 
+           plateArr.value() == 'Unlock')
+        {
+          offset =-3
+        }
         if(skillArr[t].title.includes("\n")){offset = 12;}
-        fullCard.text(skillArr[t].title,textX+28,h-(textPos-skillArr[t].startPos)+14-offset);
+        fullCard.text(skillArr[t].title,textX+28+ttoffs,h-(textPos-skillArr[t].startPos)+14-offset);
       }
       
     }
@@ -354,7 +421,7 @@ function refresh()
   fullCard.strokeWeight(10);
   fullCard.textAlign(CENTER);
   fullCard.textLeading(65);
-  fullCard.text(chName,textX,h-(namepos));
+  fullCard.text(chName,textX+ttoffs,h-(namepos));
   image(fullCard,630,0); 
   loadedImages = 0;
 }
@@ -366,11 +433,11 @@ function keyPressed()
   //loadImage('/docs/assets//renLin.png',(loadedImage)=>{cardArt=loadedImage; refresh();})
   setTimeout(() => {
     refresh();
-  }, "500");
+  }, "100");
   
 }
 
-function draw()
+function mouseClicked()
 {
-
+  refresh();
 }
